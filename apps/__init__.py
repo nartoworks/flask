e29384914ae864bbabba2ce158a6fa1b104b39f4
nartoworks <nartoworks@gmail.com>
2024-11-15 19:26:15 +0700

    edit konfigurasi agar bisa proxy

--- apps/__init__.py
+++ apps/__init__.py
@@ -3,14 +3,29 @@ import os
 from flask import Flask, Blueprint, url_for, redirect
 # from flask_mail import Mail
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.middleware.proxy_fix import ProxyFix
 import os
 from apps.config import Config
+
+class ReverseProxied(object):
+    def __init__(self, app, script_name):
+        self.app = app
+        self.script_name = script_name
+
+    def __call__(self, environ, start_response):
+        environ['SCRIPT_NAME'] = self.script_name
+        return self.app(environ, start_response)
 
 def create_app(config_class=Config):
     static_folder = os.path.join(os.path.dirname(os.path.realpath('__file__')), 'apps/static')
     template_folder = os.path.join(os.path.dirname(os.path.realpath('__file__')), 'apps/templates')
-    app = Flask(__name__, template_folder=template_folder, static_folder=static_folder, static_url_path='/static')
+    #static_folder = '/var/www/flask/static'
+    #template_folder = '/var/www/flask/templates'
+    app = Flask(__name__, template_folder=template_folder, static_folder=static_folder, static_url_path='/flask/static')
     app.config.from_object(Config)
+    app.wsgi_app = ReverseProxied(app.wsgi_app, script_name='/flask')
+    #app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1)
+    #app.config["APPLICATION_ROOT"] = '/flask'
 
     from apps.main.views import main
 
@@ -19,5 +34,6 @@ def create_app(config_class=Config):
     @app.route('/')
     def home():
         return ['Running']
+
 
     return app
